4be66bed285921c10ffbb0ac4cbef3efce41c039
Ayden939 <212651389+Ayden939@users.noreply.github.com>
2026-05-01 19:49:00 -0500

    Balance drops

--- loot.py
+++ loot.py
@@ -18,14 +18,14 @@ def loot_drop(rarity, hero, floor):
 
     if hero.weapon is None or weapon.attack > hero.weapon.attack:
         possible_drops.append(weapon)
-        weights.append(.3)
+        weights.append(.2)
 
     if hero.armor is None or armor.defense > hero.armor.defense:
         possible_drops.append(armor)
-        weights.append(.4)
+        weights.append(.2)
 
     possible_drops.append(None)
-    weights.append(.3)
+    weights.append(.6)
 
     choice = random.choices(possible_drops, weights = weights, k = 1)[0]
 
